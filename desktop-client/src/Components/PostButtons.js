b10c6a88f07b740d21c4594f94fe02377640aa37
Dan <bionicbeaver@tuta.io>
2022-09-23 14:29:36 +0100

    added debug flags for testing

--- desktop-client/src/Components/PostButtons.js
+++ desktop-client/src/Components/PostButtons.js
@@ -8,9 +8,9 @@ import { mdiThumbUp, mdiChat, mdiShare } from '@mdi/js'; // import required icon
 export const PostButtons = ({ postId }) => {
     return (
         <div className='flex-row button-group'>
-            <Button type='primary' icon={<Icon path={mdiThumbUp} size={0.75}/>}>Like</Button>
-            <Button type='primary' icon={<Icon path={mdiChat} size={0.75}/>}>Comment</Button>
-            <Button type='primary' icon={<Icon path={mdiShare} size={0.75}/>}>Share</Button>
+            <Button type='primary' clickEvent={() => {console.log(`liked post-${postId.$oid}`)}} icon={<Icon path={mdiThumbUp} size={0.75}/>}>Like</Button>
+            <Button type='primary' clickEvent={() => {console.log(`comment button clicked on post-${postId.$oid}`)}} icon={<Icon path={mdiChat} size={0.75}/>}>Comment</Button>
+            <Button type='primary' clickEvent={() => {console.log(`Shared post-${postId.$oid}`)}} icon={<Icon path={mdiShare} size={0.75}/>}>Share</Button>
         </div>
     )
 }
