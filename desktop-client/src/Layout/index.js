f51ac43117de3fa8a2af4db8cfb1ca5d6f6f3c6f
Dan <bionicbeaver@tuta.io>
2022-09-23 14:28:54 +0100

    improved layout

--- desktop-client/src/Layout/index.js
+++ desktop-client/src/Layout/index.js
@@ -1,8 +1,11 @@
+import {Link} from 'react-router-dom'
+
 import Button from '../Components/Button'
 
 // Helpers (API, icons)
 import Icon from '@mdi/react'; //import icons lib
-import {mdiMoonWaxingCrescent } from '@mdi/js'; // import required icons
+import {mdiMoonWaxingCrescent, mdiCog } from '@mdi/js'; // import required icons
+import { getUser } from '../Helpers/userHelper';
 
 
 import './css/fonts.css'
@@ -13,27 +16,48 @@ import './css/typography.css'
 import './css/positioning.css'
 import './css/colors.css'
 import './css/navigation.css'
+import { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 
 
 const Layout = (props) => {
+  const topBarSearchForm = useRef()
+  const navigate = useHistory()
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    if(topBarSearchForm.current.value.trim()){
+    navigate.push(`/search/${topBarSearchForm.current.value}`)
+  }
+  }
+  
   return (
     <>
+    <div className='top-bar flex-row flex-space-between'>
+      <div className='flex-row'>
+      <form onSubmit={handleSearch}>
+        <input ref={topBarSearchForm} className='surface' type={'text'} placeholder='Search' />
+      </form>
+      </div>
+      <div className='flex-row'>
+      Logged in User Widget here
+      </div>
+    </div>
       <nav
-        id="nav"
         title={props.brandName}>
         <div className={'flex-col'}>
 
         {props.links.map(link => {
           return (
-            <a key={link.text}
+            <Link key={link.text}
               className="nav-link"
               title={link.text}
               description={link.description}
-              href={link.href}
+              to={link.href}
               >
               {link.icon}
-              </a>
+              </Link>
           )
         })}
         </div>
@@ -47,9 +71,24 @@ const Layout = (props) => {
         }}
         title={'Toggle Theme'}
         description={'Switch between light mode and dark mode'} />
+        <Button
+        type="nav-link"
+        icon={<Icon path={mdiCog} size={1}/>}
+        clickEvent={() => {
+          if(!props.userLoggedIn.userId){
+            console.log('user logged in')
+            props.login(getUser('f4782f49-aaf0-43ae-bb20-615aaf9e3cc7').data)
+          }
+          else{
+            console.log('user logged out')
+            props.login({})
+          }
+        }}
+        title={'Toggle Login'}
+        description={'Switch logged in status'} />
         </div>
       </nav>
-      <div className="main">
+      <div className="main flex-col">
         {props.children}
       </div>
     </>
