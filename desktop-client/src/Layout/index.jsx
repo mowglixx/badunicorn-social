8216086c230d211b619f8824647302d7465f8fd9
Dan <bionicbeaver@tuta.io>
2022-09-23 16:59:14 +0100

    vite update

---
+++ desktop-client/src/Layout/index.jsx
@@ -0,0 +1,98 @@
+import {Link} from 'react-router-dom'
+
+import Button from '../Components/Button'
+
+// Helpers (API, icons)
+import Icon from '@mdi/react'; //import icons lib
+import {mdiMoonWaxingCrescent, mdiCog } from '@mdi/js'; // import required icons
+import { getUser } from '../Helpers/userHelper';
+
+
+import './css/fonts.css'
+import './css/variables.css'
+import './css/base.css'
+import './css/flexUtilities.css'
+import './css/typography.css'
+import './css/positioning.css'
+import './css/colors.css'
+import './css/navigation.css'
+import { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+
+
+
+const Layout = (props) => {
+  const topBarSearchForm = useRef()
+  const history = useHistory()
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    if(topBarSearchForm.current.value.trim()){
+    history.push(`/search/${topBarSearchForm.current.value}`)
+  }
+  }
+  
+  return (
+    <>
+    <div className='top-bar flex-row flex-space-between'>
+      <div className='flex-row'>
+      <form onSubmit={handleSearch}>
+        <input ref={topBarSearchForm} className='surface' type={'text'} placeholder='Search' />
+      </form>
+      </div>
+      <div className='flex-row'>
+      Logged in User Widget here
+      </div>
+    </div>
+      <nav
+        title={props.brandName}>
+        <div className={'flex-col'}>
+
+        {props.links.map(link => {
+          return (
+            <Link key={link.text}
+              className="nav-link"
+              title={link.text}
+              description={link.description}
+              to={link.href}
+              >
+              {link.icon}
+              </Link>
+          )
+        })}
+        </div>
+        <div className={'flex-col'}>
+
+        <Button
+        type="nav-link"
+        icon={<Icon path={mdiMoonWaxingCrescent} size={1}/>}
+        clickEvent={() => {
+          props.setDarkMode(!props.darkMode)
+        }}
+        title={'Toggle Theme'}
+        description={'Switch between light mode and dark mode'} />
+        <Button
+        type="nav-link"
+        icon={<Icon path={mdiCog} size={1}/>}
+        clickEvent={() => {
+          if(!props.userLoggedIn.userId){
+            console.log('user logged in')
+            props.login(getUser('f4782f49-aaf0-43ae-bb20-615aaf9e3cc7').data)
+          }
+          else{
+            console.log('user logged out')
+            props.login({})
+          }
+        }}
+        title={'Toggle Login'}
+        description={'Switch logged in status'} />
+        </div>
+      </nav>
+      <div className="main flex-col">
+        {props.children}
+      </div>
+    </>
+  )
+}
+
+export default Layout
---
+++ desktop-client/src/Pages/Messenger/index.jsx
@@ -0,0 +1,81 @@
+import './main.css'
+
+const Messenger = (props) => {
+  props.setTitle('Messenger')
+  return (
+    <main>
+      <div className="mainTop">
+        "Discord"
+      </div>
+      <div className="mainBottom">
+
+        <nav>
+          <div className="topNav">
+            <a href="">L</a>
+            <a href="">L</a>
+            <a href="">L</a>
+          </div>
+          <div className="bottomNav">
+            <a href="">L</a>
+          </div>
+        </nav>
+        <div className="second">
+          <div className="topBar">Channels</div>
+          <a className="pill" href="#"># general</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+          <a className="pill" href="#"># channel</a>
+        </div>
+        <div className="third">
+          <div className="topBar">
+            <div>#lorem-ipsum</div>
+            <div>
+              <a href="">D</a>
+              <a href="">V</a>
+              <a href="">U</a>
+            </div>
+          </div>
+          <div className="thread">
+
+          {/* Single Strand of conversation */}
+            <div className="strand">
+              <div className="strandHeader">
+                <div className="strandHeaderImage"><img src="https://randomuser.me/api/portraits/men/43.jpg"
+                  alt="" />
+                </div>
+                <a href="" className="strandHeaderUsername">John Smith</a>
+              </div>
+              <div className="strandBody">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, ad
+                quisquam ratione cum sapiente tempore, at tenetur voluptate incidunt id ab dicta!
+              </div>
+            </div>
+
+            <div className="conversationInput">
+              <input className="conversationInputField" type="text" placeholder="Please enter a message..." />
+            </div>
+          </div>
+          <div className="forth">
+            <div className="topBar">
+              <div>Members</div>
+            </div>
+          </div>
+
+        </div>
+        </div>
+    </main>
+  );
+}
+
+export default Messenger;
