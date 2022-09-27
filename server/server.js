4f8622e2aad8443a9023573bd5af4ab106013758
Dan <bionicbeaver@tuta.io>
2022-09-27 22:06:11 +0100

    updated package.json

--- server/server.js
+++ server/server.js
@@ -1,29 +1,54 @@
-require("./src/db/connection");
+require("./src/db/connection") // DB connection initialiser 
+const express = require("express") // The one and only
+const cors = require("cors") // Cross-Origin Resource Sharing
+
+// Route console logging
 const morgan = require('morgan')
-const express = require("express");
-const cors = require("cors");
+
+// routers
 const authRouter = require("./src/auth/routes");
 const profileRouter = require("./src/routes/profile/routes");
 const postRouter = require('./src/routes/')
-const app = express();
-const port = process.env.PORT || 3001;
 
-app.use(cors());
-app.use(express.json());
+// setup express app instance
+const app = express()
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-app.use('/auth', authRouter);
+const CONFIG = {
+  endpoint: {
+    protocol: process.env.ENDPOINT_PROTOCOL || 'http',
+    uri: process.env.ENDPOINT_URI || 'localhost',
+    port: process.env.ENDPOINT_PORT || 3001,
+  }
+}
+
+// middlewares
+app.use(cors()) // allow cross origin
+app.use(express.json()) // translate JSON automatically
+
+// log the http method, route and status 
+// with the response time when a route is 
+// visited to the console
+app.use(morgan(':method :url :status - :response-time ms'))
+
+///////////////////////////////
+// Routes                    //
+/////////////////////////////// 
+app.use('/auth', authRouter)
+
+// plans for the future
 // app.use('/profile', profileRouter);
 // app.use('/messenger', messengerRouter);
 // img / asset router?
 // app.use('/asset', assetRouter);
 
+// API status route
 app.get("/status", (req, res) => {
-  res.status(200).send({ 
-    message: "API is working" 
+  res.status(200).send({
+    message: "API is working"
   });
 });
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+// let the app listen on the defined port and 
+app.listen(CONFIG.endpoint.port, () => {
+  console.log(`Listening at ${CONFIG.endpoint.protocol}://${CONFIG.endpoint.uri}:${port}`);
 });
